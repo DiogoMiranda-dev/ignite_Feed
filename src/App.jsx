61f9a3cf605ad76e22a94cc14ea1d496142812e5
Diogo Miranda <diogosminfor@gmail.com>
2022-09-26 19:41:22 -0300

    Finalizado a parte de Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
+import { Header } from './components/Header/Header';
+import { Sidebar } from './components/Sidebar/Sidebar';
 import { Post } from './Post';
+
+import './global.css';
+import styles from './App.module.css';
 
 export function App() {
   return (
     <div>
-    <Post 
-      author="Diogo" 
-      content="lorem ipsum dolor sit amet, consectetur adipis"
-    />
-     <Post 
-      author="Miranda" 
-      content="lorem ipsum dolor sit amet, consectetur adipis lorem ipsum dolor sit amet, consectetur adipis"
-    />
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+          author="Diogo" 
+          content="lorem ipsum dolor sit amet, consectetur adipis"
+          />
+          <Post 
+          author="Miranda" 
+          content="lorem ipsum dolor sit amet, consectetur adipis lorem ipsum dolor sit amet, consectetur adipis"
+          />
+        </main>
+      </div>
     </div>
   )
 }
